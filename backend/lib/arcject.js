12b6c41badea8a9dd2203b89f104dcefda21c020
Shewale41 <shewalegopal4119@gmail.com>
2026-01-16 02:43:05 +0530

    rate limiting & bot detection completed

--- backend/lib/arcject.js
+++ backend/lib/arcject.js
@@ -1,9 +1,30 @@
-import {arcjet,shield,tokenBucket} from "@arcjet/node";
+import {arcjet,detectBot,shield,tokenBucket} from "@arcjet/node";
 
 import "dotenv/config";
 
 //init arcjet
 
 export const aj= arcjet({
-    KEY:process.env.ARCJET_KEY,
+    key:process.env.ARCJET_KEY,
+    characteristics:["ip_src"],
+    rules:[
+        //shield protects our app from xss,csrf,sql injection attacks
+        shield({mode:"LIVE"}),
+        //detects and block bots 
+        detectBot({
+            mode:"LIVE",
+            //allows only the mentioned bots and other bots will be denied
+            allow:[
+                "CATEGORY:SEARCH_ENGINE"
+            ]
+        }),
+
+        //rate limiting
+        tokenBucket({
+            mode:"LIVE",
+            refillRate:5,
+            interval:10,
+            capacity:10,
+        }),
+    ],
 })
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcject.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +22,39 @@ app.use(cors());
 
 
 app.use(morgan("dev")); //log the requests kitna time laga hai konsa response code hai karke
+
+
+//this is rate limiting ka logic 
+app.use(async (req,res,next)=>{
+    try {
+        const decision=await aj.protect(req,{
+            requested:1, //tells that each req consumes 1 token
+        });
+
+        //yaha woh teeno cases check karenge jiske liye req denied ki hai
+        //toh woh user to bata denge ki limit issue hai ,ya bot issue hai , ya kuch aur jaise ki forbidden
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                res.status(429).json({error:"Too many requests broo"});
+            }else if(decision.reason.isBot()){
+                res.status(403).json({error:"Bot access denied"});
+            }else{
+                res.status(403).json({error:"Forbidden"});
+            }
+            return; 
+        }
+
+        //check for spoofed bots - aise bots jo khudko bot nahi dikhate ya samjhte (shaane wale bots)
+        if(decision.result.some((result)=>result.reason.isBot() && result.reason.isSpoofed() )){
+            res.status(403).json({error:"Spoofed Bot detected"});
+            return;
+        }
+        next()        
+    } catch (error) {
+        console.log("Arcject error :"error);
+        next(error);
+    }
+});
 
 app.use('/api/products',productRoutes);
 
